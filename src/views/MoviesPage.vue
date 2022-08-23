7d15fc15b3e9114feb96b3d34cb2d2d902201473
Kamal Fikri Nabawi <kamalfikrinabawi@gmail.com>
2022-08-23 15:06:50 +0700

    Add Movies from Open Source API

--- src/views/MoviesPage.vue
+++ src/views/MoviesPage.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="home">
     <div class="movies-list mt-3">
-      <div class="movie mt-2" v-for="movie in items" :key="movie.id">
-        <img :src= "movie.image" alt="movieList" width="180px">
+      <div class="movie" v-for="movie in items" :key="movie.id">
+        <img :src= "movie.image" alt="movieList" width="180px" class="mt-4">
+        <div class="movie-detail">
+          <h4 class="fulltitle mb-0 text-white fw-bold">{{ movie.title }}</h4>
+          <p class="year mb-0 text-white">{{ movie.year }}</p>
+          <p class="rating mb-0 text-white">imDb Rating: {{ movie.imDbRating }}</p>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import env from '@/env.js'
 
 export default {
   name: 'MovieList',
@@ -24,12 +30,12 @@ export default {
 
   methods: {
     fetchMovieList() {
-      fetch("https://imdb-api.com/en/API/Top250Movies/k_b2gtgwdv")
-      // https://api.themoviedb.org/3/movie/76341?api_key=<<api_key>>
+      fetch(`https://imdb-api.com/en/API/MostPopularMovies/${env.apikey}`)
       .then(response => response.json())
       .then(data => {
           this.items = data.items
           console.log(data.items)
+          // console.log(data)
       });
     }
   }
@@ -37,10 +43,17 @@ export default {
 </script>
 
 <style lang="less">
-  img {
-    width: 40%;
+  // img {
+  //   width: 40%;
+  //   display: block;
+  //   margin: 0 auto;
+  // }
+
+  .movie{
     display: block;
     margin: 0 auto;
+    align-items: center;
+    text-align: center;
   }
 
 
