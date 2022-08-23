db102140533502d00505f6de0aab045ab0d149ca
Kamal Fikri Nabawi <kamalfikrinabawi@gmail.com>
2022-08-23 13:34:40 +0700

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,37 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <div class="nontonkuy-text fw-bold">
+        <router-link to="/">
+          <span class="nonton">Nonton</span>
+          <span class="kuy">Kuy</span>
+        </router-link>
+      </div>
     </nav>
     <router-view/>
   </div>
 </template>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+    background-color: #3C425C;
+  }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+.nontonkuy-text {
+    font-size: 20pt;
+    text-align: center;
+    a {
+      text-decoration: none;
+    }
+    .nonton {
+      color: #F37984;
+    }
+    .kuy {
+      color: #F2C59B;
     }
   }
-}
 </style>
---
+++ src/views/MoviesPage.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home">
+    <div class="movies-list mt-3">
+      <div class="movie mt-2" v-for="movie in items" :key="movie.id">
+        <img :src= "movie.image" alt="movieList" width="180px">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'MovieList',
+  data () {
+    return {
+      items: {}
+    }
+  },
+
+  mounted () {
+    this.fetchMovieList()
+  },
+
+  methods: {
+    fetchMovieList() {
+      fetch("https://imdb-api.com/en/API/Top250Movies/k_b2gtgwdv")
+      // https://api.themoviedb.org/3/movie/76341?api_key=<<api_key>>
+      .then(response => response.json())
+      .then(data => {
+          this.items = data.items
+          console.log(data.items)
+      });
+    }
+  }
+}
+</script>
+
+<style lang="less">
+  img {
+    width: 40%;
+    display: block;
+    margin: 0 auto;
+  }
+
+
+</style>
